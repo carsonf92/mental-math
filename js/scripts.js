89e1d52d938efa59e622d4c86c14b5b326a59f78
Carson Ford <carsonf92@gmail.com>
2020-05-25 08:24:01 -0400

    build

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,8 @@ ready(function() {
 var gameState = {
 	discipline: 'addition',
 	mode: 'practice',
+	equationArray: [0, 0, 0],
+	equationAnswers: [0, 0, 0],
 	equationCount: 0,
 	correctCount: 0,
 	timer: 60
@@ -38,63 +40,87 @@ document.addEventListener('mouseup', function() {
 });
 
 // NAV LISTENERS //
-navListener('press-start', startScreen);
+
+navListener('press-start', function() { goToScreen('start') });
+navListener('view-highscores', function() { goToScreen('highscores') });
 
 navListener('addition', function() {
 	gameState.discipline = 'addition';
-	gametypeScreen();
+	goToScreen('gametype');
 });
 navListener('subtraction', function() {
 	gameState.discipline = 'subtraction';
-	gametypeScreen();
+	goToScreen('gametype');
 });
 navListener('multiplication', function() {
 	gameState.discipline = 'multiplication';
-	gametypeScreen();
+	goToScreen('gametype');
 });
 navListener('division', function() {
 	gameState.discipline = 'division';
-	gametypeScreen();
+	goToScreen('gametype');
 });
 
 navListener('practice', function() {
 	gameState.mode = 'practice';
-	practiceScreen();
+	startSession();
+	goToScreen('gameplay');
 });
 navListener('challenge', function() {
 	gameState.mode = 'challenge';
-	challengeScreen();
+	startSession();
+	goToScreen('gameplay');
 });
 
 navListener('resign', function() {
-	titleScreen();
+	goToScreen('title');
 	document.getElementById('prompt').classList.remove('active');
 });
 navListener('dismiss', function() {
 	document.getElementById('prompt').classList.remove('active');
 });
 
+
+// NAV FUNCTIONS //
+
 // reusable keyup listeners
 function navListener(elementID, navFunction) {
 	document.getElementById(elementID).addEventListener('keyup', function(e) {
-		if (e.keyCode === 88) { // x
+		if (e.keyCode === 13 || e.keyCode === 88) { // enter or x
 			navFunction();
 		}
 	});
 }
 
-// NAV FUNCTIONS //
+// go to a screen
+function goToScreen(targetScreen) {
+	if (document.querySelector('section.active').id === 'title') {
+		document.querySelector('body').classList.remove('title');
+	}
+
+	if (targetScreen === 'title') {
+		document.querySelector('body').classList.add('title');
+	}
+
+	document.querySelector('section.active').classList.remove('active');
+	document.getElementById(targetScreen).classList.add('active');
+
+	if (targetScreen !== 'highscores' || targetScreen) {
+		document.querySelector('section.active input:checked').focus();
+	}
+}
+
 // go back a section
 document.addEventListener('keyup', function(e) {
-	if (e.keyCode === 90) { // z
+	if (e.keyCode === 27 || e.keyCode === 90) { // esc or z
 		var activeSection = document.querySelector('section.active');
 
-		if (activeSection.id === 'start') {
-			titleScreen();
+		if (activeSection.id === 'start' || activeSection.id === 'highscores') {
+			goToScreen('title');
 		}
 
 		if (activeSection.id === 'gametype') {
-			startScreen();
+			goToScreen('start');
 		}
 
 		if (activeSection.id === 'gameplay') {
@@ -104,51 +130,17 @@ document.addEventListener('keyup', function(e) {
 	}
 });
 
-function titleScreen() {
-	document.querySelector('body').classList.add('title');
-	document.querySelector('section.active').classList.remove('active');
-	document.getElementById('title').classList.add('active');
-	document.getElementById('press-start').focus();
-}
-
-function startScreen() {
-	document.querySelector('body').classList.remove('title');
-	document.querySelector('section.active').classList.remove('active');
-	document.getElementById('start').classList.add('active');
-	document.getElementById('addition').focus();
-}
-
-function gametypeScreen() {
-	document.querySelector('section.active').classList.remove('active');
-	document.getElementById('gametype').classList.add('active');
-	document.getElementById('practice').focus();
-}
-
-function practiceScreen() {
-	document.querySelector('section.active').classList.remove('active');
-	document.getElementById('gameplay').classList.add('active');
-	document.querySelector('#answers span:first-child').focus();
-	gameState.mode = 'practice';
-	startSession(gameState.discipline, gameState.mode);
-}
-
-function challengeScreen() {
-	document.querySelector('section.active').classList.remove('active');
-	document.getElementById('gameplay').classList.add('active');
-	document.querySelector('#answers span:first-child').focus();
-	gameState.mode = 'challenge';
-	startSession(gameState.discipline, gameState.mode);
-}
-
 
 // ========================
 // Gameplay
 // ========================
 
-function startSession(discipline, mode) {
-	// if challenge start countdown
+function startSession() {
+	if (gameState.mode === 'challenge') {
+		// start countdown
+	}
 
-	// generate first equation
+	generateEquation(gameState.discipline);
 }
 
 function correctAnswer() {
@@ -179,14 +171,104 @@ function endSession() {
 // Equation Generators
 // ========================
 
-// Addition
+function generateEquation(discipline) {
+	switch (discipline) {
+		case 'addition':
+			additionEquation();
+			break;
+		case 'subtraction':
+			subtractionEquation();
+			break;
+		case 'multiplication':
+			multiplicationEquation();
+			break;
+		case 'division':
+			divisionEquation();
+			break;
+	}
 
-// Subtraction
+	generateAnswers();
+}
 
-// Multiplication
+function additionEquation() {
+	gameState.equationArray[0] = randomIntFromInterval(1, 100);
+	gameState.equationArray[1] = randomIntFromInterval(1, (100 - gameState.equationArray[0]));
+	gameState.equationArray[2] = gameState.equationArray[0] + gameState.equationArray[1];
 
-// Division
+	// randomize order of addends
+	if (randomIntFromInterval(0, 1)) {
+		var temp = gameState.equationArray[0];
+		gameState.equationArray[0] = gameState.equationArray[1];
+		gameState.equationArray[1] = temp;
+	}
 
+	document.getElementById('operator').innerHTML = '+';
+	document.getElementById('digit-one').innerHTML = gameState.equationArray[0];
+	document.getElementById('digit-two').innerHTML = gameState.equationArray[1];
+}
+
+function subtractionEquation() {
+
+}
+
+function multiplicationEquation() {
+
+}
+
+function divisionEquation() {
+
+}
+
+// create random answers and place as options
+function generateAnswers() {
+	// correct answer
+	gameState.equationAnswers[0] = gameState.equationArray[2];
+	
+	// wrong answer one
+	do {
+		gameState.equationAnswers[1] = randomIntFromInterval(0, 100);
+	} while (gameState.equationAnswers[1] === gameState.equationAnswers[0]);
+
+	// wrong answer two
+	do {
+		gameState.equationAnswers[2] = randomIntFromInterval(0, 100);
+	} while (gameState.equationAnswers[2] === gameState.equationAnswers[0] || gameState.equationAnswers[2] === gameState.equationAnswers[1]);
+
+	// shuffle answers
+	shuffle(gameState.equationAnswers);
+
+	// place answers into buttons
+	document.querySelector('#answer-one + span').innerHTML = gameState.equationAnswers[0];
+	document.querySelector('#answer-two + span').innerHTML = gameState.equationAnswers[1];
+	document.querySelector('#answer-three + span').innerHTML = gameState.equationAnswers[2];
+}
+
+// random int of range
+function randomIntFromInterval(min, max) { 
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// shuffle elements in an array
+function shuffle(array) {
+	var currentIndex = array.length,
+		temporaryValue,
+		randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 
 
